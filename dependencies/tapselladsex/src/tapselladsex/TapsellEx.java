301610f295ca27a5e0b3ceeabe6aeb0d741b64b8
Evgeny Rovkin <evgeny.rovkin@nevosoft.ru>
2020-07-09 19:12:21 +0600

    first commit

---
+++ dependencies/tapselladsex/src/tapselladsex/TapsellEx.java
@@ -0,0 +1,258 @@
+package tapselladsex;
+
+import android.provider.Settings.Secure;
+import android.util.Log;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+import android.opengl.GLSurfaceView;
+
+import org.haxe.extension.Extension;
+import org.haxe.lime.HaxeObject;
+
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
+
+import ir.tapsell.plus.AdRequestCallback;
+import ir.tapsell.plus.TapsellPlus;
+import ir.tapsell.plus.AdShowListener;
+import ir.tapsell.sdk.Tapsell;
+import ir.tapsell.sdk.TapsellShowOptions;
+import ir.tapsell.sdk.TapsellAdRequestListener;
+import ir.tapsell.sdk.TapsellAdRequestOptions;
+import ir.tapsell.sdk.TapsellAdShowListener;
+
+
+public class TapsellEx extends Extension { 
+
+	private static Boolean failRewarded=false;
+	private static Boolean loadingRewarded=false;
+	private static String rewardedId= null;
+	private static String adId = null;
+
+	private static TapsellEx instance=null;
+	private static Boolean testingAds=false;
+
+	private static HaxeObject callback=null;
+
+	public static final String FAILED = "FAILED";
+	public static final String CLOSED = "CLOSED";
+	public static final String DISPLAYING = "DISPLAYING";
+	public static final String LOADED = "LOADED";
+	public static final String LOADING = "LOADING";
+	public static final String EARNED_REWARD = "EARNED_REWARD";
+
+	private TapsellEx() {
+		if (testingAds) {
+			String android_id = Secure.getString(mainActivity.getContentResolver(), Secure.ANDROID_ID);
+			String deviceId = md5(android_id).toUpperCase();
+			Log.d("TapsellEx","DEVICE ID: " + deviceId);
+			
+			TapsellPlus.addFacebookTestDevice(deviceId);
+		}
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Log.d("TapsellEx", "reloadRewarded");
+				TapsellEx.getInstance().reloadRewarded(TapsellEx.rewardedId);
+			}
+		}, 5000);
+	}
+
+	public static TapsellEx getInstance() {
+		if (TapsellEx.instance == null) TapsellEx.instance = new TapsellEx();
+		return TapsellEx.instance;
+	}
+
+	public static void init(String rewardedId, String appId, boolean testingAds, HaxeObject callback){
+		TapsellEx.rewardedId=rewardedId;
+		TapsellEx.testingAds=testingAds;
+		TapsellEx.callback=callback;
+
+		Log.d("TapsellEx ","init");
+
+        //TapsellPlus.initialize(mainActivity, appId);
+        Tapsell.initialize(mainActivity, appId);
+
+		mainActivity.runOnUiThread(new Runnable() {
+			public void run() { getInstance(); }
+		});
+	}
+
+	private static void reportRewardedEvent(final String event) {
+		if (callback == null) return;
+
+		if (Extension.mainView == null) return;
+		GLSurfaceView view = (GLSurfaceView) Extension.mainView;
+		view.queueEvent(new Runnable() {
+			public void run() {
+				Log.d("TapsellEx ","reportRewardedEvent" + event);
+				callback.call("_onRewardedEvent", new Object[] { event } );
+			}
+		});
+	}
+
+	public static boolean showRewarded(final String rewardedId) {
+		if (loadingRewarded) return false;
+
+		if (failRewarded){
+			mainActivity.runOnUiThread(new Runnable() {
+				public void run() {
+					getInstance().reloadRewarded(TapsellEx.rewardedId);
+				}
+			});
+			Log.d("TapsellEx showRewarded","Show Rewarded: Rewarded not loaded... reloading.");
+			return false;
+		}
+
+		if (TapsellEx.rewardedId=="") {
+			Log.d("TapsellEx showRewarded","Show Rewarded: RewardedID is empty... ignoring.");
+			return false;
+		}
+
+		mainActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+
+					//AdShowListener adShowListener = new AdShowListener() {
+					TapsellAdShowListener adShowListener = new TapsellAdShowListener() {
+						@Override
+						public void onOpened() {
+							Log.d("Tapsell", "Ad Displaying");
+							reportRewardedEvent(TapsellEx.DISPLAYING);
+						}
+						@Override
+						public void onRewarded(boolean completed) { ///
+							Log.d("Tapsell", "Reward");
+							reportRewardedEvent(TapsellEx.EARNED_REWARD);
+						}
+						@Override
+						public void onClosed() {
+							Log.d("Tapsell", "Ad Closed");
+							reportRewardedEvent(TapsellEx.CLOSED);
+							
+							new Handler().postDelayed(new Runnable() {
+								@Override
+								public void run() {
+									TapsellEx.getInstance().reloadRewarded(TapsellEx.rewardedId);
+								}
+							}, 5000);
+						}
+						
+						@Override
+						public void onError(String s) {
+							Log.d("Tapsell", "error :" + s);
+							TapsellEx.getInstance().failRewarded = true;
+							reportRewardedEvent(TapsellEx.FAILED);
+						}
+					};
+					
+					int rotation = mainActivity.getWindowManager().getDefaultDisplay().getRotation();
+					Log.d("Tapsell rotation", String.valueOf(rotation));
+					Log.d("Tapsell orientation", String.valueOf(mainActivity.getResources().getConfiguration().orientation));
+					
+					TapsellShowOptions showOptions = new TapsellShowOptions();
+					if (rotation == 3) showOptions.setRotationMode(TapsellShowOptions.ROTATION_LOCKED_REVERSED_LANDSCAPE);
+					else showOptions.setRotationMode(TapsellShowOptions.ROTATION_LOCKED_LANDSCAPE);
+
+					Tapsell.showAd(mainActivity,
+							TapsellEx.rewardedId,
+							TapsellEx.adId,
+							showOptions,
+							adShowListener
+							);
+
+					/*
+					TapsellPlus.showAd(
+						mainActivity,
+						TapsellEx.rewardedId,
+						adShowListener
+					);
+					*/
+
+			}
+		});
+
+		return true;
+	}
+
+	private void reloadRewarded(String rewardedId){
+		if(rewardedId=="") return;
+		if(loadingRewarded) return;
+		Log.d("TapsellEx","Reload Rewarded");
+
+		loadingRewarded=true;
+		failRewarded=false;
+		
+		reportRewardedEvent(TapsellEx.LOADING);
+
+		/*
+		AdRequestCallback adRequestCallback = new AdRequestCallback() {
+			@Override
+			public void response() {
+				Log.d("Tapsell", "Ad Response");
+				TapsellEx.getInstance().loadingRewarded=false;
+				reportRewardedEvent(TapsellEx.LOADED);
+			}
+			@Override
+			public void error(String message) {
+				Log.e("Tapsell error", message);
+				TapsellEx.getInstance().loadingRewarded=false;
+				TapsellEx.getInstance().failRewarded=true;
+				reportRewardedEvent(TapsellEx.FAILED);
+			}
+		};
+		
+		TapsellPlus.requestRewardedVideo(
+			mainActivity,
+			TapsellEx.rewardedId,
+			adRequestCallback
+		);
+		*/
+
+		Tapsell.requestAd(mainActivity,
+			TapsellEx.rewardedId,
+			new TapsellAdRequestOptions(),
+			new TapsellAdRequestListener() {
+
+				@Override
+				public void onAdAvailable(String adId) {
+					Log.d("tapsell", "AdAvailable");
+
+					TapsellEx.getInstance().adId = adId;
+					TapsellEx.getInstance().loadingRewarded=false;
+				
+					reportRewardedEvent(TapsellEx.LOADED);
+				}
+
+				@Override
+				public void onError(String message) {
+
+					TapsellEx.getInstance().loadingRewarded=false;
+					TapsellEx.getInstance().failRewarded=true;
+					reportRewardedEvent(TapsellEx.FAILED);
+					
+					Log.d("tapsell", "ad error" + message);
+					
+				}
+			});
+		
+	}
+
+	private static String md5(String s)  {
+		MessageDigest digest;
+		try  {
+			digest = MessageDigest.getInstance("MD5");
+			digest.update(s.getBytes(),0,s.length());
+			String hexDigest = new java.math.BigInteger(1, digest.digest()).toString(16);
+			if (hexDigest.length() >= 32) return hexDigest;
+			else return "00000000000000000000000000000000".substring(hexDigest.length()) + hexDigest;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+}
---
+++ extension/tapsellads/TapsellAds.hx
@@ -0,0 +1,121 @@
+package extension.tapsellads;
+
+import haxe.Json;
+import nme.Lib;
+import nme.JNI;
+
+
+class TapsellAds {
+
+	private static var initialized:Bool=false;
+	private static var testingAds:Bool=false;
+
+	private static var __init:String->String->Bool->Dynamic->Void = function(rewardedId:String, appId:String, testingAds:Bool, callback:Dynamic){};
+
+	private static var __showRewarded:String->Bool = function(rewardedId:String){ return false; };
+
+	private static var completeCB:Void->Void;
+	private static var skipCB:Void->Void;
+	private static var rewardFlag:Bool;
+	private static var _rewardedId:String;
+	private static var _appId:String;
+	private static var canshow:Bool = false;
+
+	public static function canShowAds():Bool {
+		return canshow;
+	}
+
+	public static function showRewarded(cb, skip):Bool {
+		completeCB = cb;
+		skipCB = skip;
+		canshow = false;
+
+		try{
+			return __showRewarded(TapsellAds._rewardedId);
+		}catch(e:Dynamic){
+			trace("ShowRewarded Exception: "+e);
+		}
+		return false;
+	}
+	
+	public static function enableTestingAds() {
+		if ( testingAds ) return;
+		if ( initialized ) {
+			var msg:String;
+			msg = "FATAL ERROR: If you want to enable Testing Ads, you must enable them before calling INIT!.\n";
+			msg+= "Throwing an exception to avoid displaying read ads when you want testing ads.";
+			trace(msg);
+			throw msg;
+			return;
+		}
+		testingAds = true;
+	}
+
+	public static function init(rewardedId:String, appId:String){
+
+		TapsellAds._rewardedId = rewardedId;
+		TapsellAds._appId = appId;
+
+		if (initialized) return;
+		initialized = true;
+
+		#if android
+		try{
+			__init = JNI.createStaticMethod("tapselladsex/TapsellEx", "init", "(Ljava/lang/String;Ljava/lang/String;ZLorg/haxe/lime/HaxeObject;)V");
+			__showRewarded = JNI.createStaticMethod("tapselladsex/TapsellEx", "showRewarded", "(Ljava/lang/String;)Z");
+			__init(rewardedId, appId, testingAds, instance);
+		}catch(e:Dynamic){
+			trace("Android INIT Exception: "+e);
+		}
+		#end
+	}
+	
+	public static inline var FAILED:String = "FAILED";
+	public static inline var CLOSED:String = "CLOSED";
+	public static inline var LOADED:String = "LOADED";
+	public static inline var EARNED_REWARD:String = "EARNED_REWARD";
+
+	public static var onRewardedEvent:String->Void = null;
+	private static var instance:TapsellAds = new TapsellAds();
+
+	private function new(){}
+
+	public function _onEvent(event:String){
+		
+	}
+
+	public function _onRewardedEvent(event:String) {
+		if (onRewardedEvent != null) {
+			try{
+				
+				trace("tapsell onRewardedEvent", event);
+
+				switch (event)
+				{
+					case LOADED:
+						canshow = true;
+
+					case EARNED_REWARD:
+						rewardFlag = true;
+
+					case FAILED:
+						skipCB();
+						rewardFlag = false;
+						canshow = false;
+
+					case CLOSED:
+						rewardFlag ? completeCB() : skipCB();
+						rewardFlag = false;
+						canshow = false;
+				}
+
+				onRewardedEvent(event);
+			}
+			catch(err:Dynamic){
+				trace("tapsell ERROR PARSING ", " err : ", err);
+			}
+		}
+		else trace("Rewarded event: "+event+ " (assign TapsellAds.onRewardedEvent to get this events and avoid this traces)");
+	}
+	
+}
